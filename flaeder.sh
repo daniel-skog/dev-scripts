649f6bcf474785b696bdb273e562ecc613fec357
daniel-skog <jda.skog@gmail.com>
2015-09-13 21:31:40 +0200

    Fixed start for image from docker hub

--- flaeder.sh
+++ flaeder.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 
-docker run -ti --rm -e DISPLAY=$DISPLAY -u developer --name flaeder-sublime -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v `pwd`:/home/developer/project -w /home/developer dev-tools /bin/bash -c "./start.sh $1"
+docker run -ti --rm -e DISPLAY=$DISPLAY -u developer --name flaeder-devtools -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v `pwd`:/home/developer/project -w /home/developer flaederstudios/dev-tools:latest /bin/bash -c "./start.sh $1"
