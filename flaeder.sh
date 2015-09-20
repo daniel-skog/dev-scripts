dac55827ccda7e9155a74324b24a7696e0c479a4
daniel-skog <jda.skog@gmail.com>
2015-09-20 21:47:07 +0200

    Added more tools...

--- flaeder.sh
+++ flaeder.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -ti --rm -e DISPLAY=$DISPLAY -u developer --name flaeder-devtools -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v `pwd`:/project -v $HOME:$HOME -w /project flaederstudios/dev-tools:latest /bin/bash -c "./start.sh $1"
+docker run -ti --rm -e HOME=$HOME -e DISPLAY=$DISPLAY -e LANG:$LANG --name flaeder-devtools \
+-v $HOME:$HOME:rw -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v `pwd`:/home/developer/project:rw -w /home/developer/project \
+flaederstudios/dev-tools:latest /bin/bash -c "/home/developer/start.sh $1"
