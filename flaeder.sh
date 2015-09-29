b48a636fffe95b164c40660f1fe315c2620e6f60
daniel-skog <jda.skog@gmail.com>
2015-09-29 20:04:40 +0200

    Now passing flags to container

--- flaeder.sh
+++ flaeder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -ti --rm -e HOME=$HOME -e DISPLAY=$DISPLAY -e LANG:$LANG --name flaeder-devtools \
+docker run -ti --rm -e HOME=$HOME -e DISPLAY=$DISPLAY -e LANG:$LANG \
 -v $HOME:$HOME:rw -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v `pwd`:/home/developer/project:rw -w /home/developer/project \
-flaederstudios/dev-tools:latest /bin/bash -c "/home/developer/start.sh $1"
+flaederstudios/dev-tools:latest /bin/bash -c "/home/developer/start.sh $1 $2 $3 $4 $5 $6"
